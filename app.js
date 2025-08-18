7d46755c3659b0d1b21d0b5fc3aa615231a12524
angentidea <angentidea@gmail.com>
2025-08-18 10:29:05 +0100

    adds controllers, debugs, adds data

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require ('cors')
-const app = express()
 
-app.request(cors())
+const logger = require('./middleware/logger')
+
+const app = express()
+app.use(cors())
+app.use(logger)
 
 module.exports = app
